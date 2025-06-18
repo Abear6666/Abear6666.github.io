09dbd5bad7a4b72072b90fec104796baee78bf3c
LUO\PC <2540567665@qq.com>
2025-06-18 16:52:38 +0800

    Site updated: 2025-06-18 16:52:38

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/06/18/hello-world/","2025/06/18/测试文章/"];function toRandomPost(){
+var posts=["2025/06/18/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
